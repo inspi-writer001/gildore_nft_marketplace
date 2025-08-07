87200acaf5bdf98ca52342662f153fb8d1ac2725
inspi-writer001 <conservedinnovation@gmail.com>
2025-08-07 08:39:24 +0100

    feature: implemented fee for burning

--- programs/anchor_marketplace/src/instructions/redeem.rs
+++ programs/anchor_marketplace/src/instructions/redeem.rs
@@ -1,9 +1,8 @@
-use anchor_lang::prelude::*;
-use mpl_core::{
-    accounts::{BaseAssetV1, BaseCollectionV1},
-    instructions::{BurnV1CpiBuilder, TransferV1CpiBuilder},
-    types::UpdateAuthority,
+use anchor_lang::{
+    prelude::*,
+    system_program::{transfer, Transfer},
 };
+use mpl_core::{accounts::BaseAssetV1, instructions::BurnV1CpiBuilder, types::UpdateAuthority};
 
 use crate::{Listing, Marketplace, MarketplaceError};
 
@@ -17,6 +16,13 @@ pub struct RedeemNFT<'info> {
         constraint = asset.key() == listing.mint @ MarketplaceError::AssetMismatch,
     )]
     pub asset: Account<'info, BaseAssetV1>,
+
+    #[account(
+        mut,
+        seeds = [b"treasury", marketplace.key().as_ref()],
+        bump = marketplace.treasury_bump,
+    )]
+    pub treasury: SystemAccount<'info>,
 
     #[account(
         mut,
@@ -49,6 +55,30 @@ pub struct RedeemNFT<'info> {
 
 impl<'info> RedeemNFT<'info> {
     pub fn redeem_nft(&mut self) -> Result<()> {
+        let token_price = self.listing.get_price_by_token_id();
+        let marketplace_fee = self.marketplace.fee_bps as u64;
+
+        // Pay half purchase fee to redeem physically
+        let amount_to_transfer_as_fee = token_price
+            .checked_mul(marketplace_fee)
+            .and_then(|mul_result| mul_result.checked_div(10_000))
+            .ok_or_else(|| error!(MarketplaceError::MathOverflowError))?;
+
+        let half_amount = amount_to_transfer_as_fee
+            .checked_div(2)
+            .ok_or_else(|| error!(MarketplaceError::MathOverflowError))?;
+
+        // Transfer fee to treasury
+        let cpi_account_fee_ix = Transfer {
+            from: self.owner.to_account_info(),
+            to: self.treasury.to_account_info(),
+        };
+
+        transfer(
+            CpiContext::new(self.system_program.to_account_info(), cpi_account_fee_ix),
+            half_amount,
+        )?;
+
         let listing = &self.listing.key();
         let signers_seeds: &[&[&[u8]]] = &[&[b"escrow", listing.as_ref()]];
 
